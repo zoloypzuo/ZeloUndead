8f8d8e6f21a56bbfcc7f8608dd06873a2bc12f97
zoloypzuo <910181598@qq.com>
2022-02-04 22:58:52 +0800

    update ai script

--- Src/AI/AIState.cs
+++ Src/AI/AIState.cs
@@ -14,7 +14,6 @@ public abstract class AIState : MonoBehaviour
 	// Default Handlers
 	public virtual void			OnEnterState()			{}
 	public virtual void 		OnExitState()			{}
-	public virtual void 		OnAnimatorUpdated() 	{}
 	public virtual void 		OnAnimatorIKUpdated()	{}
 	public virtual void 		OnTriggerEvent( AITriggerEventType eventType, Collider other ){}
 	public virtual void 		OnDestinationReached ( bool isReached ) {}
@@ -25,4 +24,24 @@ public abstract class AIState : MonoBehaviour
 
 	// Protected Fields
 	protected AIStateMachine	_stateMachine;
+
+	// ------------------------------------------------------------------
+	// Name	:	OnAnimatorUpdated
+	// Desc	:	Called by the parent state machine to allow root motion
+	//			processing
+	// ------------------------------------------------------------------
+	public virtual void 		OnAnimatorUpdated() 	
+	{
+		// Get the number of meters the root motion has updated for this update and
+		// divide by deltaTime to get meters per second. We then assign this to
+		// the nav agent's velocity.
+		if (_stateMachine.useRootPosition)
+			_stateMachine.navAgent.velocity = _stateMachine.animator.deltaPosition / Time.deltaTime;
+
+		// Grab the root rotation from the animator and assign as our transform's rotation.
+		if (_stateMachine.useRootRotation)
+			_stateMachine.transform.rotation = _stateMachine.animator.rootRotation;
+
+	}
+
 }
