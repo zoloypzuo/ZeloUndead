9663f561ec9ab06d6b3bd555fe7edd29b7580e96
zoloypzuo <910181598@qq.com>
2022-02-04 22:58:17 +0800

    ai script

---
+++ Src/AI/AIState.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// ----------------------------------------------------------------------
+// Class	:	AIState
+// Desc		:	The base class of all AI States used by our AI System.
+// ----------------------------------------------------------------------
+public abstract class AIState : MonoBehaviour 
+{
+	// Public Method
+	// Called by the parent state machine to assign its reference
+	public void SetStateMachine( AIStateMachine stateMachine ) { _stateMachine = stateMachine; }
+
+	// Default Handlers
+	public virtual void			OnEnterState()			{}
+	public virtual void 		OnExitState()			{}
+	public virtual void 		OnAnimatorUpdated() 	{}
+	public virtual void 		OnAnimatorIKUpdated()	{}
+	public virtual void 		OnTriggerEvent( AITriggerEventType eventType, Collider other ){}
+	public virtual void 		OnDestinationReached ( bool isReached ) {}
+
+	// Abtract Methods
+	public abstract AIStateType GetStateType();
+	public abstract AIStateType OnUpdate();
+
+	// Protected Fields
+	protected AIStateMachine	_stateMachine;
+}
---
+++ Src/AI/AIZombieStateMachine.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Doesn't do anything yet
+public class AIZombieStateMachine : AIStateMachine
+{
+
+}
