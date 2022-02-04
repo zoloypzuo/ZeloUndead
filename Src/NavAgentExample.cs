fd8920f5e02e012a8e01d83b6efe6a86944f4df6
zoloypzuo <910181598@qq.com>
2022-02-04 12:38:42 +0800

    Update NavAgentExample.cs

--- Src/NavAgentExample.cs
+++ Src/NavAgentExample.cs
@@ -15,6 +15,7 @@ public class NavAgentExample : MonoBehaviour
 	public bool				 PathPending	 = false;
 	public bool				 PathStale		 = false;
 	public NavMeshPathStatus PathStatus      = NavMeshPathStatus.PathInvalid;
+	public AnimationCurve	 JumpCurve		 = new AnimationCurve();
 
 	// Private Members
 	private NavMeshAgent _navAgent = null;
@@ -37,6 +38,7 @@ public class NavAgentExample : MonoBehaviour
 		// If not valid Waypoint Network has been assigned then return
 		if (WaypointNetwork==null) return;
 
+		// Set first waypoint
 		SetNextDestination ( false );
 	}
 
@@ -53,10 +55,11 @@ public class NavAgentExample : MonoBehaviour
 
 		// Calculatehow much the current waypoint index needs to be incremented
 		int incStep = increment?1:0;
+		Transform nextWaypointTransform = null;
 
 		// Calculate index of next waypoint factoring in the increment with wrap-around and fetch waypoint 
 		int nextWaypoint = (CurrentIndex+incStep>=WaypointNetwork.Waypoints.Count)?0:CurrentIndex+incStep;
-		Transform nextWaypointTransform = WaypointNetwork.Waypoints[nextWaypoint];
+		nextWaypointTransform = WaypointNetwork.Waypoints[nextWaypoint];
 
 		// Assuming we have a valid waypoint transform
 		if (nextWaypointTransform!=null)
@@ -69,7 +72,7 @@ public class NavAgentExample : MonoBehaviour
 		}
 
 		// We did not find a valid waypoint in the list for this iteration
-		CurrentIndex++;
+		CurrentIndex=nextWaypoint;
 	}
 
 	// ---------------------------------------------------------
@@ -84,13 +87,58 @@ public class NavAgentExample : MonoBehaviour
 		PathStale	= _navAgent.isPathStale;
 		PathStatus	= _navAgent.pathStatus;
 
+		// If agent is on an offmesh link then perform a jump
+		if (_navAgent.isOnOffMeshLink)
+		{
+			StartCoroutine( Jump( 1.0f) );
+			return;
+		}
+
 		// If we don't have a path and one isn't pending then set the next
 		// waypoint as the target, otherwise if path is stale regenerate path
-		if ( (!HasPath && !PathPending) || PathStatus==NavMeshPathStatus.PathInvalid /*|| PathStatus==NavMeshPathStatus.PathPartial*/)
+		if ( ( _navAgent.remainingDistance<=_navAgent.stoppingDistance && !PathPending) || PathStatus==NavMeshPathStatus.PathInvalid /*|| PathStatus==NavMeshPathStatus.PathPartial*/)
+		{
 			SetNextDestination ( true );
-		else
+		}
+			else
 		if (_navAgent.isPathStale)
 			SetNextDestination ( false );
+	}
 
+	// ---------------------------------------------------------
+	// Name	:	Jump
+	// Desc	:	Manual OffMeshLInk traversal using an Animation
+	//			Curve to control agent height.
+	// ---------------------------------------------------------
+	IEnumerator Jump ( float duration )
+	{
+		// Get the current OffMeshLink data
+		OffMeshLinkData 	data 		= _navAgent.currentOffMeshLinkData;
+
+		// Start Position is agent current position
+		Vector3				startPos	= _navAgent.transform.position;
+
+		// End position is fetched from OffMeshLink data and adjusted for baseoffset of agent
+		Vector3				endPos		= data.endPos + ( _navAgent.baseOffset * Vector3.up);
+
+		// Used to keep track of time
+		float 				time		= 0.0f;
+
+		// Keeo iterating for the passed duration
+		while ( time<= duration )
+		{
+			// Calculate normalized time
+			float t = time/duration;
+
+			// Lerp between start position and end position and adjust height based on evaluation of t on Jump Curve
+			_navAgent.transform.position = Vector3.Lerp( startPos, endPos, t ) + (JumpCurve.Evaluate(t) * Vector3.up) ;
+
+			// Accumulate time and yield each frame
+			time += Time.deltaTime;
+			yield return null;
+		}
+
+		// All done so inform the agent it can resume control
+		_navAgent.CompleteOffMeshLink();
 	}
 }
