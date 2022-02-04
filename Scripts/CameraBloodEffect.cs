eeebfd9cd99c3dd99ccb1405e8afefce4c24e13f
zoloypzuo <910181598@qq.com>
2022-02-04 23:29:31 +0800

    mv scripts

---
+++ Scripts/CameraBloodEffect.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode()]
+public class CameraBloodEffect : MonoBehaviour 
+{
+	[SerializeField]
+	private Shader 		_shader = null;
+	private Material	_material = null;
+
+	void OnRenderImage( RenderTexture src, RenderTexture dest )
+	{
+		if (_shader==null) return;
+		if (_material==null)
+		{
+			_material = new Material( _shader );
+		}
+
+		if (_material==null) return;
+
+		Graphics.Blit( src, dest, _material);
+	}
+
+}
