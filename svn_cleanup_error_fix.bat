edc612a840929c6e0bf4f15612999d219716835d
zoloypzuo <910181598@qq.com>
2022-02-15 13:52:59 +0800

    svn

---
+++ svn_cleanup_error_fix.bat
@@ -0,0 +1,7 @@
+rem fix svn cleanup error
+rem Failed to run the WC DB work queue associated
+
+sqlite3 .svn/wc.db "select * from work_queue"
+sqlite3 .svn/wc.db "delete from work_queue"
+sqlite3 .svn/wc.db "select * from work_queue"
+pause complete
