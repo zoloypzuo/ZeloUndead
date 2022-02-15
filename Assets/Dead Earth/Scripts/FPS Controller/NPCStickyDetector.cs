0874b18d5cba4d5c77abdc7c7ed18dc4369c7658
左亦 <910181598@qq.com>
2022-02-15 14:28:08 +0800

    Create Project (#8)
    * Update .gitignore
    
    * rm scripts
    
    * Update .svngitignore
    
    * Update .gitignore
    
    * idea
    
    * Update .gitignore
    
    * scripts
    
    * Update .svngitignore
    
    * svn
    
    * Update .gitignore
    
    * Create .gitignore
    
    * setting

---
+++ Assets/Dead Earth/Scripts/FPS Controller/NPCStickyDetector.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// ------------------------------------------------------------------------------------------------
+// CLASS	:	NPCStickyDetector
+// DESC		:	Should be added to a child game object of the FPS controller and be assigned
+//				a layer which generates collision/trigger messages with the AI Entity Layer.
+//				It instructs the FPS controller to apply stickiness and also override the
+//				zombie's visual threat and current state
+// -------------------------------------------------------------------------------------------------
+public class NPCStickyDetector : MonoBehaviour 
+{
+
+	FPSController _controller = null;
+
+	// Use this for initialization
+	void Start () 
+	{
+		_controller = GetComponentInParent<FPSController>();	
+	}
+
+	void OnTriggerStay( Collider col )
+	{
+		// Is this collier a zombie
+		AIStateMachine machine = GameSceneManager.instance.GetAIStateMachine( col.GetInstanceID());
+		if (machine!=null && _controller!=null)
+		{
+			// Apply stickiness
+			_controller.DoStickiness();
+
+			// Set THIS location as the zombie's visual threat
+			machine.VisualThreat.Set( AITargetType.Visual_Player,
+									  _controller.characterController, 
+									  _controller.transform.position, 
+									  Vector3.Distance( machine.transform.position, _controller.transform.position)
+									 );
+
+			// Force the zombie in ATTACK state
+			machine.SetStateOverride( AIStateType.Attack );
+
+		}
+	}
+
+}
